9c50ff77ae667a70a0450342c31e69b75222f4a8
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-02-15 21:59:47 -0300

    Create Aula03_Ex98_rascunhoTodosCodigosAula01.R

---
+++ Aula03_Ex98_rascunhoTodosCodigosAula01.R
@@ -0,0 +1,35 @@
+# i <- 1
+# while (i < 10) {
+# print(i)
+# i <- i + 1
+# }
+
+# i <- 1
+# while (i < 10) {
+#   print(i)
+#   i <- i + 1
+#   if (i == 5) {
+#     break
+#   }
+# }
+
+# x <- 1:5 
+# for (val in x) { 
+# if (val == 3){ 
+# next 
+# } 
+# print(val) 
+# } 
+
+# #Exemplo 01
+
+# for (n in 0:4) {
+# n <- n + 1
+# print(n)
+# }
+
+# #Exemplo 02
+# for (n in 1:5) {
+# M=5*n
+# print(M)
+# }
