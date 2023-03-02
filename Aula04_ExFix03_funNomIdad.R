af8ed3643ba3be517d8fe81963b019a623baefc4
Gui Celestino <86801366+Guikki@users.noreply.github.com>
2023-03-01 21:27:57 -0300

    Update Aula04_ExFix03_funNomIdad.R

--- Aula04_ExFix03_funNomIdad.R
+++ Aula04_ExFix03_funNomIdad.R
@@ -1,4 +1,12 @@
+#Aqui, uma forma mais simples, mantendo o padrão de impressão do R
+# my_function <- function(nome,idade) {
+# print(paste("Meu nome é ",nome,", e minha idade é", idade))
+# }
+# my_function("Maria",26)
+
+
+#E aqui, com o sep tirando o espaço entre o nome e a vírgula
 my_function <- function(nome,idade) {
-print(paste("Meu nome é ",nome,", e minha idade é", idade))
+print(paste("Meu nome é ",nome, ", e minha idade é", idade, sep=""))
 }
 my_function("Maria",26)
